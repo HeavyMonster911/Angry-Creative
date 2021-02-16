486c79c6974a3608797b695f75f656b4e39b7f65
HeavyMonster911 <72610595+HeavyMonster911@users.noreply.github.com>
2021-02-16 14:43:03 +0200

    Create js.js

---
+++ js.js
@@ -0,0 +1,9 @@
+function letsStart() {
+    stop:for (i = 0; i <= 1000; i++) {
+        let signal = prompt("Text something")
+        if (!signal) break stop
+    }
+    
+    alert("Access denied")
+}
+
